f1fa965e078189fe7b1610d6a4e2cf6213d45769
Austin <harrenaustint@gmail.com>
2021-05-25 16:18:37 -0400

    push

--- server.js
+++ server.js
@@ -1,45 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose')
-
+const mongoose = require('mongoose');
 
 require('dotenv').config();
-
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
-// app.use(express.static('client/build'))
 app.use(express.json());
-// fs.readdir("./routes", (err, files) => {
-//   files.forEach(file => {
-//   app.use("/", require("./routes/" + file))
-//   });
-//   })
+
 const uri = process.env.ATLAS_URI;
-        
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
-
-
 connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
-const exercisesRouter = require('./routes/exercises');  
-const usersRouter = require('./routes/users');  
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 
-app.use('/exercises', exercisesRouter);  
+app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
-
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
 app.listen(port, () => {
-console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
