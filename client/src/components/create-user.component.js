ceaee4f60092bc6efd5a8771845a4407106843a1
Austin <harrenaustint@gmail.com>
2021-05-25 13:43:45 -0400

    push

---
+++ client/src/components/create-user.component.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import { BACKEND_URL } from '../config';
+
+export default class CreateUser extends Component {
+
+  constructor(props){
+    super(props)
+  
+    this.onChangeUsername= this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state={
+      username: ''
+    }
+  }
+
+  onChangeUsername(e){
+    this.setState({
+      username: e.target.value
+    })
+  }
+
+  onSubmit(e){
+    e.preventDefault();
+    const newUser = {
+      username: this.state.username
+    }
+    console.log(newUser)
+    //axios.post takes two arguments
+    //1. url to post to as a string
+    //2. what we are posting
+    axios.post(BACKEND_URL + '/users/add', newUser)
+      .then((res)=>{
+        console.log(res.data)
+      })
+    this.setState({
+      username: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+            type="text"
+            required
+            className="form-control"
+            value={this.state.username}
+            onChange={this.onChangeUsername}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="create user"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
