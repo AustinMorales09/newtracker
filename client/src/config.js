0b9d3651fe8bcb27533bec6fdfc3af302161847e
Austin <harrenaustint@gmail.com>
2021-05-25 15:35:59 -0400

    wor

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 import dotenv from 'dotenv'
 dotenv.config();
-export const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://localhost:5000" : "http://git.heroku.com/deploy09.git"
+export const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://localhost:5000" : "http://stoopid-trcker.herokuapp.com/"
